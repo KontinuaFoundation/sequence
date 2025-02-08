60a53c66136c6cf75e84d9c9369a36d6e4d17066
Aaron Hillegass <gtg129u@gatech.edu>
2025-02-08 17:00:17 -0500

    Added numerical integration for double integrals

---
+++ Chapters/numerical_multi_int/num_integration.py
@@ -0,0 +1,50 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+
+# pip3 install scipy
+weight_lower_limit = 3.0
+weight_upper_limit = 4.0
+weight_slices = 100
+
+diameter_lower_limit = 1.5
+diameter_upper_limit = 2.0
+diameter_slices = 100
+
+# What's the average weight and diameter
+mean_vector = np.array([4.35559489,2.3526593 ])
+print(f"Mean [weight, diameter] = {mean_vector}")
+
+# Do heavier snails tend to have bigger shells?
+covariance_matrix = np.array([[1.33099714,0.44309754],
+ [0.44309754,0.41603925]])
+print(f"Covariance = \n{covariance_matrix}")
+
+# Create a multivariate normal distribution
+rv = multivariate_normal(mean_vector, covariance_matrix)
+
+delta_weight = (weight_upper_limit - weight_lower_limit) / weight_slices
+delta_diameter = (diameter_upper_limit - diameter_lower_limit) / diameter_slices
+
+sum = 0.0
+# Step though each different weight
+for i in range(weight_slices):
+    # What is the weight in the middle of this slice?
+    current_weight = weight_lower_limit + (i + 0.5) * delta_weight
+
+    for j in range(diameter_slices):
+        # What is the diameter in the middle of this slice?
+        current_diameter = diameter_lower_limit + (j + 0.5) * delta_diameter
+
+        # What is the probability density there?
+        prob_density = rv.pdf((current_weight, current_diameter))
+
+        # What is the volume under that for this tiny square
+        sum += prob_density * delta_weight * delta_diameter
+
+print(
+    f"\bThe probability that the weight is between {weight_lower_limit} and {weight_upper_limit}"
+)
+print(
+    f"and that the diameter is between {diameter_lower_limit} and {diameter_upper_limit}"
+)
+print(f"is about {sum * 100.0:.4f}%")
