cac11ba92676de54a0867df7524ec88f5e3d24d3
Arjan Ellingson <67170142+asellingson28@users.noreply.github.com>
2025-07-17 13:10:47 -0500

    fixes and reorginization

--- Build/make_topic_index.py
+++ Build/make_topic_index.py
@@ -20,8 +20,10 @@ with open("user.cfg", "r") as config_fd:
 # Gather all metadatas    
 book_nums = [str(x).zfill(2) for x in range(1, vol_count + 1)]
 books_metadata = []
+duplicates = [] # ideally empty
+sequencePrereqError = [] # ideally empty
 
-# EVERY topics with references and videos
+# every topic (only)
 all_topics = {}
 for book in book_nums:
     print(f"Parsing resources for {book}...")
@@ -30,7 +32,19 @@ for book in book_nums:
     # topics = all "covers" from that book
     (book_metadatas, topics) = util.gather_data("../Chapters", book, config)
     books_metadata.append(book_metadatas)
-    
+
+    # duplicate checker (WORKING)
+    # previously was: # all_topics.update(topics)
+
+    for topic_key, tdict in topics.items():
+        if topic_key in all_topics:                         # seen before
+            first_chap = all_topics[topic_key]['chap_id']   # original
+            this_chap  = tdict['chap_id']                   # duplicate
+            duplicates.append((topic_key, first_chap, this_chap))
+            # print(f"⚠️ DUPLICATE topicID '{topic_key}': {first_chap} → {this_chap}") PRINTING after
+        else:                                               # first sighting
+            all_topics[topic_key] = tdict                   # store as-is
+
     # check prerequisites before adding new topics (logical order)
     for chapter in book_metadatas:
         chap_title = chapter.get('title', 'Unknown Title')
@@ -38,16 +52,19 @@ for book in book_nums:
         requires = chapter.get('requires', [])
         for req in requires:
             if req not in all_topics:
-                print(f"🛑🛑🛑 {req} required in chapter {chap_title} but not covered previously")
+                sequencePrereqError.append((chap_title, req));
+                # print(f"🛑🛑🛑 {chap_title} requires {req} but not covered previously")
 
-    # duplicate checker (WORKING)
-    # previously was: # all_topics.update(topics)
-    for key in topics:
-        if key in all_topics:
-            print(f"⚠️⚠️⚠️ DUPLICATE TOPIC: {key}")
-        else:
-            all_topics[key] = topics[key]
+print("--------------")
+for item in sequencePrereqError:
+    print(f"🛑🛑🛑 {item[0]} requires {item[1]} but not covered previously")
+   
+print("--------------")
 
+for d in duplicates:
+    print(f"⚠️ DUPLICATE TOPIC {d[0]}: origin: {d[1]} → duplicated in: {d[2]}")
+
+print("--------------")
 
 out_dict = {}
 for topic, tdict in all_topics.items():
