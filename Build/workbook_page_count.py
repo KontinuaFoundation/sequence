5867f82406ad60bfe2469e14477ad06e6b9e4cf4
Arjan Ellingson <67170142+asellingson28@users.noreply.github.com>
2026-03-20 16:26:33 -0700

    charge: progres on chapter; reordering

---
+++ Build/workbook_page_count.py
@@ -0,0 +1,20 @@
+import re
+mod_dir = "../../Chapters"
+vol_count = 36
+
+# script assumes that build workbooks all has been run, st all book aux files exist
+
+for i in range(1, vol_count + 1):
+    book_num = str(i).zfill(2)
+    path_for_aux = f"./Intermediate/workbook-{book_num}.aux"
+    aux_file = open(path_for_aux, "r")
+    aux = aux_file.read()
+    aux_file.close()
+
+    search_string = r"@abspage@last\{(\d+)\}" # \@abspage@last{63} where 63 is the num of pages
+    match = re.search(search_string, aux)
+    if match: 
+        num1 = int(match.groups()[0])
+        print(f"{book_num} -- {num1}")
+    else:
+        print("-1")
