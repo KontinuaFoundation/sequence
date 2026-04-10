27d40024e1668f3f170c018df66f20e0b9d338a9
Pranav Gundu <pranav@gundu.me>
2026-04-10 17:50:22 -0400

    ci: change directory

--- Build/concat_chaps.bash
+++ Build/concat_chaps.bash
@@ -1,6 +1,6 @@
-cd ../Chapters || exit 1
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR/../Chapters" || exit 1
 : > book_00.txt
 for i in $(seq -w 1 36); do
   cat "book_$i.txt" >> book_00.txt
 done
-cd ../Build || exit 1
