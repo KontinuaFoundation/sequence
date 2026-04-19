4d0bf5d928d948b1bb45a918843221af37b7dc14
Pranav Gundu <pranav@gundu.me>
2026-04-18 20:39:37 -0400

    fix(build): handle missing aux file in workbook_page_count

--- Build/workbook_page_count.py
+++ Build/workbook_page_count.py
@@ -1,20 +1,19 @@
+import os
 import re
-mod_dir = "../../Chapters"
-vol_count = 36
 
-# script assumes that build workbooks all has been run, st all book aux files exist
+vol_count = 36
+search_re = re.compile(r"@abspage@last\{(\d+)\}")
 
 for i in range(1, vol_count + 1):
     book_num = str(i).zfill(2)
-    path_for_aux = f"./Intermediate/workbook-{book_num}.aux"
-    aux_file = open(path_for_aux, "r")
-    aux = aux_file.read()
-    aux_file.close()
-
-    search_string = r"@abspage@last\{(\d+)\}" # \@abspage@last{63} where 63 is the num of pages
-    match = re.search(search_string, aux)
-    if match: 
-        num1 = int(match.groups()[0])
-        print(f"workbook {book_num} -- {num1} pages")
+    aux_path = f"./Intermediate/workbook-{book_num}.aux"
+    if not os.path.exists(aux_path):
+        print(f"workbook {book_num} -- missing {aux_path}")
+        continue
+    with open(aux_path, "r") as f:
+        aux = f.read()
+    match = search_re.search(aux)
+    if match:
+        print(f"workbook {book_num} -- {int(match.group(1))} pages")
     else:
-        print("-1")
+        print(f"workbook {book_num} -- -1")
